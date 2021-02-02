4cb12ca3af8ed1c87fee231afdaac3fd70968d3f
aimee-liang <aimeeeliang@gmail.com>
2021-02-02 17:58:37 -0500

    Added comments

--- index.ts
+++ index.ts
@@ -4,14 +4,26 @@ let newArray : any = [1, 2, 3]
 
 newArray = "me"
 
+// strong type classes
 interface Person {
     first: string,
     last: string,
-    age: number
+    age: number,
+    [key: string]: any
 }
 
 const person1: Person = {
     first: "Beyonce",
     last: "Knowles-Carter",
-    age: 39
+    age: 39,
+    profession: "Singer-Actress-Business Mogul"
 }
+
+//strong type functions
+
+function multiply(x: number, y: number){
+    return (x * y)
+}
+
+// strong type arrays
+
