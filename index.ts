5d47b69c6592ad39511cf1b0ea123d5d8412f887
aimee-liang <aimeeeliang@gmail.com>
2021-02-02 17:52:07 -0500

    Learning how to create interface and classes

--- index.ts
+++ index.ts
@@ -4,3 +4,14 @@ let newArray : any = [1, 2, 3]
 
 newArray = "me"
 
+interface Person {
+    first: string,
+    last: string,
+    age: number
+}
+
+const person1: Person = {
+    first: "Beyonce",
+    last: "Knowles-Carter",
+    age: 39
+}
